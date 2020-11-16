fd2d1ac8d44a8e3043a6e7c4ebf7d8ab2cc51ac0
Taiwo Adepoju <tadepoju95@gmail.com>
2020-11-16 15:58:08 -0600

    Modified all files in project folder.

--- recipe.js
+++ recipe.js
@@ -3,8 +3,7 @@ let fetchRecipes = () => {
         .then(response => {
             let recipes = response.data;
             console.log(`GET recipes`, recipes);
-            displayRecipe(recipes);
-            
+            displayRecipe(recipes);            
         })
         .catch(error => console.error(error));
 }
@@ -15,6 +14,32 @@ let displayRecipe = (recipes) => {
 	let getName = getMeals.strMeal;
 	let divElement = document.getElementsByClassName('randomRecipe')[0].innerHTML = getName;
 	let getImage = document.getElementById('myImg').src = getMeals.strMealThumb;
+	let getInstructions = document.getElementById('instructions').innerHTML = getMeals.strInstructions;
+	let grabVideo = getMeals.strYoutube;
+	let createElement = "<button onclick='video("+ JSON.stringify(grabVideo) + ")' id='videoElement'>" + "watch Video"  + "</button>" 
+    let displayVideoButton = document.getElementById('getVideo').innerHTML = createElement;
+    let setIngredients = document.getElementById('listIngredients');
+    let ulElement = '<ul>';
+   	for(let i = 1; i <= 20; i++) {
+   		if(getMeals[`strIngredient${i}`]) {
+   			ulElement += '<li>' + `${getMeals[`strIngredient${i}`]} - ${getMeals[`strMeasure${i}`]}` + '</li>';
+   		}
+   	}
+   	 ulElement += '</ul>'
+	 setIngredients.innerHTML = ulElement;
 }
 
+let video = (grabVideo) => {
+	if(grabVideo !== '') {
+		location.href = grabVideo;
 
+	} else {
+		document.getElementById('setInnerhtml').innerHTML = 'Video Unavailable';
+	}
+}
+
+let getRecipe = () => {
+	fetchRecipes();
+	document.getElementById('setInnerhtml').innerHTML = '';
+
+}
