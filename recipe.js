06d6381b02a830b46ba0b432256ae4a7259fb54d
Taiwo Adepoju <tadepoju95@gmail.com>
2020-11-24 02:24:47 -0600

    Modified all files in project folder.

--- recipe.js
+++ recipe.js
@@ -2,7 +2,7 @@ let fetchRecipes = () => {
 	axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
         .then(response => {
             let recipes = response.data;
-            console.log(`GET recipes`, recipes);
+            //console.log(`GET recipes`, recipes);
             displayRecipe(recipes);            
         })
         .catch(error => console.error(error));
@@ -14,9 +14,8 @@ let displayRecipe = (recipes) => {
 	let divElement = document.getElementsByClassName('randomRecipe')[0].innerHTML = getName;
 	let getImage = document.getElementById('myImg').src = getMeals.strMealThumb;
 	let getInstructions = document.getElementById('instructions').innerHTML = getMeals.strInstructions;
-	let grabVideo = getMeals.strYoutube;
-	let createElement = "<button onclick='video("+ JSON.stringify(grabVideo) + ")' id='videoElement'>" + "watch Video"  + "</button>" 
-    let displayVideoButton = document.getElementById('getVideo').innerHTML = createElement;
+	let videoUrl = JSON.stringify(getMeals.strYoutube);
+	video(videoUrl);
     let setIngredients = document.getElementById('listIngredients');
     let ulElement = '<ul>';
    	for(let i = 1; i <= 20; i++) {
@@ -28,17 +27,16 @@ let displayRecipe = (recipes) => {
 	 setIngredients.innerHTML = ulElement;
 }
 
-let video = (grabVideo) => {
-	if(grabVideo !== '') {
-		window.open(grabVideo);
-
-	} else {
-		document.getElementById('setInnerhtml').innerHTML = 'Video Unavailable';
-	}
+let video = (videoUrl) => {
+	if(videoUrl !== "\"\"" ) {
+      setVideoButton = "<a href=" + videoUrl + " target='_blank'><button id='videoElement'>" + "watch video" + "</button></a>"
+	  document.getElementById('getVideo').innerHTML = setVideoButton;
+    } else {
+      setVideoButton = "<button id='videoElement'>" + "video unavailable" + "</button>"
+      document.getElementById('getVideo').innerHTML = setVideoButton;
+    }
 }
 
 let getRecipe = () => {
 	fetchRecipes();
-	document.getElementById('setInnerhtml').innerHTML = '';
-
 }
